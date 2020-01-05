1731f8d2fe3c8d7cfcd203c9a642bc25b106ab3e
Rifai <abdelrahman.rifai@gmail.com>
2020-01-05 19:19:06 +0100

    connect apollo client to React

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+})
 
 function App() {
   return (
-    <div>
-      <h1>Hello Boovies!</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Hello Boovies!</h1>
+      </div>
+    </ApolloProvider>
   )
 }
 
